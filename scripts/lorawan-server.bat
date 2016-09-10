aee0703cd5f80865bff2a715fffd572d7f93834f
Petr Gotthard <petr.gotthard@centrum.cz>
2016-09-10 19:46:56 +0200

    Fix portability to other Erlang versions (issue #3)

---
+++ scripts/lorawan-server.bat
@@ -0,0 +1,14 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set SCRIPT_DIR=%~dp0
+for %%A in ("%SCRIPT_DIR%\..") do (
+    set "ROOT_DIR=%%~fA"
+)
+
+for /D %%A in (%ROOT_DIR%\lib\*) do (
+    set FILES=!FILES! %%A\ebin
+)
+
+cd %ROOT_DIR% && erl -noinput +Bd -sname lorawan -pa !FILES! -s lorawan_app -config releases/0.1.0/sys.config
