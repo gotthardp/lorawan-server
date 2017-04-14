3580d29c1114875370ce91b787f661395cd442fb
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2017-04-14 04:11:57 +0200

    Improvements for docker: debuglevel and google api key

--- priv/admin/admin-config.js
+++ priv/admin/admin-config.js
@@ -1,4 +1,5 @@
-/* Google API key, which has Google Maps enabled. */
+/* Google API key, which has Google Maps enabled.
+   set GOOGLE_MAPS_KEY environment variable to have this configured automatically */
 var GoogleMapsKey = "";
 
 /* end of file */
