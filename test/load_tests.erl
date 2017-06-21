a0634101622aa5943f056437991e7d6c5114acb0
Petr Gotthard <petr.gotthard@centrum.cz>
2017-06-21 21:56:38 +0200

    By default do very modest load tests

--- test/load_tests.erl
+++ test/load_tests.erl
@@ -7,7 +7,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define(GW_COUNT, 10).
--define(NODES_PER_GW, 100).
+-define(NODES_PER_GW, 10).
 -define(FRAMES_PER_NODE, 5).
 
 -define(NWKSKEY, <<"2B7E151628AED2A6ABF7158809CF4F3C">>).
