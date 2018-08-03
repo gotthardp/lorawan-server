e1f9077215cf4eea431b09bb38bbbb67b9cbad3b
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2018-08-03 18:34:55 +0200

    Fix scripts

--- scripts/dbexport.bat
+++ scripts/dbexport.bat
@@ -10,7 +10,7 @@ set AUTH=--user=admin --password=admin
 set DIR=backup-%AA%-%BB%-%CC%
 
 mkdir %DIR%
-for %%G in (user,area,gateway,multicast_channel,network,group,profile,device,node,ignored_node,handler,connector) do (
+for %%G in (config,users,areas,gateways,multicast_channels,networks,groups,profiles,devices,nodes,ignored_nodes,handlers,connectors) do (
     echo Processing db %%G
     wget -nv %AUTH% --output-document=%DIR%/%%G.json http://%SERVER%/api/%%G
 )
