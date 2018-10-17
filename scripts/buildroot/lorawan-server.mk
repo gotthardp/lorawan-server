30651d611e64b40229948ae79387c2e55741c607
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2018-10-17 17:53:13 +0200

    Issue #501: Draft Buildroot scripts

---
+++ scripts/buildroot/lorawan-server.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# lorawan-server
+#
+################################################################################
+
+LORAWAN_SERVER_VERSION = v0.6.2
+LORAWAN_SERVER_SITE = $(call github,gotthardp,lorawan-server,$(LORAWAN_SERVER_VERSION))
+LORAWAN_SERVER_LICENSE = MIT
+LORAWAN_SERVER_LICENSE_FILES = LICENSE
+LORAWAN_SERVER_INSTALL_STAGING = YES
+
+define LORAWAN_SERVER_BUILD_CMDS
+	$(MAKE) -C $(@D) $(HOST_CONFIGURE_OPTS) release
+endef
+
+define LORAWAN_SERVER_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(generic-package))
