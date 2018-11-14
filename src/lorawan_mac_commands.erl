515c4365e2230511f55c0881a7c3dff2993d852f
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2018-11-14 09:30:32 +0100

    Issue #516: Fix crash when time is calculated

--- src/lorawan_mac_commands.erl
+++ src/lorawan_mac_commands.erl
@@ -280,7 +280,7 @@ time_to_gps({Date, {Hours, Min, Secs}}) ->
     TotalSecs = calendar:datetime_to_gregorian_seconds({Date, {Hours, Min, trunc(Secs)}})
             - calendar:datetime_to_gregorian_seconds({{1980, 1, 6}, {0, 0, 0}})
             + 17, % leap seconds
-    1000*TotalSecs + (Secs - trunc(Secs)).
+    trunc(1000*(TotalSecs + (Secs - trunc(Secs)))). % ms
 
 
 auto_adr(Network, #profile{adr_mode=1}=Profile, #node{adr_flag=1, adr_failed=Failed}=Node)
